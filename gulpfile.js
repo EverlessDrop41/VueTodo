ab7bdf90a758e55b63a09a027dbc8f4da5bb0522
emilyperegrine <peregrineemily@gmail.com>
2016-05-17 20:56:35 +0100

    Added minify css to gulp

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+// grab our gulp packages
+var gulp    = require('gulp');
+var gutil   = require('gulp-util');
+var sass    = require('gulp-sass');
+var cssmin  = require('gulp-cssmin');
+var rename  = require('gulp-rename');
+var webpack = require('webpack-stream');
+
+var paths = {
+  sass: 'sass/**/*.scss',
+  webpack: 'app/**/*.js',
+  css: 'public/css/app.css'
+}
+
+gulp.task('sass', function () {
+  return gulp.src(paths.sass)
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('webpack', function () {
+  return gulp.src(paths.webpack)
+    .pipe(webpack( require('./webpack.config.js') ))
+    .pipe(gulp.dest('public/scripts'));
+});
+
+gulp.task('mincss', function () {
+  return gulp.src(paths.css)
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('default', ['sass', 'mincss', 'webpack'], function() {
+  return gutil.log('gulped')
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.webpack, ['webpack']);
+  gulp.watch(paths.sass, ['sass', 'mincss']);
+});
